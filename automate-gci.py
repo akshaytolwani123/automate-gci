edab8b3d5d7c7ec94fd28c9f606fda9ef57b9da3
Akshay <akshaytolwanipython@gmail.com>
2019-12-15 11:06:13 +0530

    added startupadd.py

--- automate-gci.py
+++ automate-gci.py
@@ -2,9 +2,6 @@ import tkinter as tk
 import  os
 import webbrowser
 
-file = open(".bashrc","a")
-file.write("echo Running at boot\n")
-file.write("python3 automate-gci.py")
 
 
 root = tk.Tk()
@@ -13,7 +10,7 @@ root.title("Automate 3 functions")
 def github():
     webbrowser.open_new_tab("github.com")
 
-button1 = tk.Button(root,text="open github",width= 25,command = github())
+button1 = tk.Button(root,text="open github",width= 25,command = github)
 button1.pack()
 
 def code():
@@ -25,7 +22,7 @@ button2.pack()
 def checkmail():
     webbrowser.open_new_tab("gmail.com/inbox")
 
-button3 = tk.Button(root,text="Check your email",width= 25,command =checkmail() )
+button3 = tk.Button(root,text="Check your email",width= 25,command =checkmail )
 button3.pack()
 
 
